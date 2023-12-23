10ba43180c74e6fba5842193c3a66e56002037cd
Mahmoud AHMED <84628337+mahmoud2344@users.noreply.github.com>
2023-12-23 21:12:02 +0100

    Update Real_time_occupancy_matrix.py

--- Real_time_occupancy_matrix.py
+++ Real_time_occupancy_matrix.py
@@ -12,67 +12,91 @@ Description: This code Uses the same efficient algorithm from our simulation,
     The program Uses RPLIDAR data, create the occupancy matrix using specified density threshold.
 """
 
+# Import necessary libraries
 from rplidar import RPLidar
 import numpy as np
+import keyboard
 
+# Set the port name for the RPLIDAR device
 PORT_NAME = 'COM4'
 
 # User inputs
-occupancy_threshold = 0.2
-coef = 100
-num_cells = 8
-num_slices = 12
-real_max_radius = 8000
-screen_max_radius = 400
+occupancy_threshold = 0.2  # Threshold for occupancy matrix
+coef = 100  # Coefficient for occupancy matrix calculation
+num_cells = 8  # Number of cells in the matrix
+num_slices = 12  # Number of slices in the matrix
+real_max_radius = 8000  # Maximum radius in real-world units
+screen_max_radius = 400  # Maximum radius for screen visualization
 
+# Calculate angle step and angles for each slice
 angle_step = 360 / num_slices
 angles = np.deg2rad(np.arange(0, 360, angle_step))
 
+# Calculate distances for screen visualization and real-world distances
 circle_distance = screen_max_radius / num_cells
-
 real_circle_distance = real_max_radius / num_cells
 
+# Define the size of the occupancy matrix
 matrix_size = (num_cells, num_slices)
 
+# Calculate the area for each cell and store it in a list
 list1 = []
 for cell_idx in range(1, num_cells + 1):
-    # Calculate the area for the current cell
     area = ((cell_idx * circle_distance) ** 2 * np.pi - ((cell_idx - 1) * circle_distance) ** 2 * np.pi) / num_slices
     list1.append(area)
 
+# Display the list of areas
 print(list1)
 
 
+# Define the main function for running the LIDAR scan
 def run():
+    # Initialize the RPLIDAR device
     lidar = RPLidar(PORT_NAME)
     try:
         running = True
+        # Continuously read LIDAR scans
         for scan in lidar.iter_scans():
             matrix = np.zeros(matrix_size)
 
+            # Process each data point in the scan
             for (_, angle, distance) in scan:
                 dis = int(distance // real_circle_distance)
-                ang = int((360-angle) // angle_step) % num_slices
+                ang = int((360 - angle) // angle_step) % num_slices
                 if 0 <= dis < num_cells:
                     matrix[dis, ang] += 1
+
+            # Display the occupancy matrix as integers
             print(f"occupancy:{matrix.astype(int)}")
 
+            # Convert the occupancy matrix to a binary matrix based on the threshold
             matrix = np.array(matrix)
-
             occupancy_matrix = np.zeros_like(matrix, dtype=float)
             occupancy_matrix = matrix * coef / np.array(list1)[:num_cells, None] > occupancy_threshold
 
+            # Display the binary occupancy matrix as integers
             print(occupancy_matrix.astype(int))
 
+            # Check for 'q' key press to stop the program
+            if keyboard.is_pressed('q'):
+                print("Stopping the program...")
+                running = False
+                break
+
+            # Check for KeyboardInterrupt to stop the program
             if not running:
                 break
 
     except KeyboardInterrupt:
         pass
 
-    lidar.stop()
-    lidar.disconnect()
+    finally:
+        # Stop the lidar motor and disconnect the RPLIDAR device
+        lidar.stop_motor()
+        lidar.stop()
+        lidar.disconnect()
 
 
+# Run the main function if the script is executed directly
 if __name__ == '__main__':
     run()
